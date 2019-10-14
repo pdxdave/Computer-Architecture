440a4baef1e5cb92ed269008c2b0f54e412c0934
pdxdave <webdevdave@hotmail.com>
2019-10-14 15:49:13 -0700

    more work on the cpu document

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,9 +7,20 @@ class CPU:
 
 
     def __init__(self):
+        """Also add properties for any internal registers you need, e.g. `PC`."""
         """Construct a new CPU."""
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.pc = 0
+
+    def ram_read(self, MAR):
+        """ should accept the address to read and return the value stored there."""
+        return self.ram
+
+
+    def ram_write(self, MAR):
+        """should accept a value to write, and the address to write it to."""
+        return self.ram
 
     def load(self):
         """Load a program into memory."""
@@ -34,10 +45,12 @@ class CPU:
 
 
     def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+        """ALU operations. Arithmetic Logic Unit.  OK, addition, subraction, multiplcation goes here """
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+        elif op == 'MUL':
+            self.reg[reg_a] += self.reg[reb_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -63,5 +76,14 @@ class CPU:
         print()
 
     def run(self):
+        """This code above requires the implementation of three instructions: LDI, PRN, HLT. values from cheat sheet"""
+        """IR, the Instruction Register. This can just be a local variable in run()."""
         """Run the CPU."""
-        pass
+        HLT = 0b00000001             """ Halt the CPU (and exit the emulator)."""
+        LDI = 0b10000010             """ Set the value of a register to an integer."""
+        PRN = 0b1000111              """ Print numeric value stored in the given register."""
+        PUSH = 0b01000101            """ Push the value in the given register on the stack."""
+        POP = 0b01000110             """ Pop the value at the top of the stack into the given register."""
+        CALL = 0b01010000            """ Calls a subroutine (function) at the address stored in the register. """
+        RET = 0b00010001             """ Return from subroutine. """
+        MUL = 0b10100010             """ Multiply the values in two registers together and store the result in registerA. """
