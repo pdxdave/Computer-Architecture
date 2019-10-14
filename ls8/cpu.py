d5df51f12a475ee12d76b91e8ae6368832cdbf28
pdxdave <webdevdave@hotmail.com>
2019-10-14 14:27:18 -0700

    first change to cpu file

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,9 +5,11 @@ import sys
 class CPU:
     """Main CPU class."""
 
+
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
 
     def load(self):
         """Load a program into memory."""
