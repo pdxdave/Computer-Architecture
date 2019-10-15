2e4fd32c2a519fc3b52abcd226bb145a3533263b
pdxdave <webdevdave@hotmail.com>
2019-10-14 20:47:31 -0700

    started over and got it working

--- ls8/old_cpu.py
+++ ls8/old_cpu.py
@@ -1,29 +1,29 @@
 """CPU functionality."""
- # Round Two on this assignment.  Make it easier
 
 import sys
-
 
 class CPU:
     """Main CPU class."""
 
+
     def __init__(self):
+        """Also add properties for any internal registers you need, e.g. `PC`."""
         """Construct a new CPU."""
+        """ R7 is reserved as the stack pointer (SP). These registers only hold values between 0-255. """
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
-        self.SP = 247
+        self.SP = 7
+        self.reg[7] = 255
 
     def ram_read(self, MAR):
         """ should accept the address to read and return the value stored there."""
-        # MAR contains the address that is being read or written to
-        return self.ram[MAR]  
+        return self.ram
 
 
-    def ram_write(self, MAR, MDR):
+    def ram_write(self, MAR):
         """should accept a value to write, and the address to write it to."""
-        # remember, MDR is the data that was read or the data to write
-        self.ram[MAR] = MDR  
+        return self.ram
 
     def load(self):
         """Load a program into memory."""
@@ -48,7 +48,7 @@ class CPU:
 
 
     def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+        """ALU operations. Arithmetic Logic Unit.  OK, addition, subtraction, multiplication goes here """
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
@@ -78,17 +78,24 @@ class CPU:
 
         print()
 
+    
+
     def run(self):
+        """This code above requires the implementation of three instructions: LDI, PRN, HLT. values from cheat sheet"""
+        """IR, the Instruction Register. This can just be a local variable in run()."""
         """Run the CPU."""
-        HLT  = 0b00000001  
-        LDI  = 0b10000010  
-        PRN  = 0b01000111  
-        PUSH = 0b01000101  
-        POP  = 0b01000110  
-        MUL  = 0b10100010  
+        HLT = 0b00000001             """ Halt the CPU (and exit the emulator)."""
+        LDI = 0b10000010             """ Set the value of a register to an integer."""
+        PRN = 0b1000111              """ Print numeric value stored in the given register."""
+        PUSH = 0b01000101            """ Push the value in the given register on the stack. Think I'll need a method for this"""
+        POP = 0b01000110             """ Pop the value at the top of the stack into the given register. Think I'll need a method for this"""
+        CALL = 0b01010000            """ Calls a subroutine (function) at the address stored in the register. """
+        RET = 0b00010001             """ Return from subroutine. """
+        MUL = 0b10100010             """ Multiply the values in two registers together and store the result in registerA. """
 
         running = True
         while running:
+
             # this comes right out of the instructions. It talks about reading the memory
             # address stored in the register 'pc' and storing it in the 'ir'
             IR = self.ram[self.pc]
@@ -96,18 +103,23 @@ class CPU:
             # operand a and b
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
-            # if the IR is Halted, this isn't running 
+
+            # now we have to go through 8 instructions seen above.
+            
+            # if the Instruction Register is Halted, this isn't running 
             if IR == HLT:
                 running = False
+            # elsif the integer is the same as (will use IR from here on) the IR
             elif IR == LDI:
                 self.reg[operand_a] = operand_b
-                self.pc += 3
+                self.pc 
             # elif print  equals IR
             elif IR == PRN:
                 print(self.reg[operand_a])
-                self.pc += 2
-            else:
-                print("Halting the program")
-                running = False
+                self.pc 
+            elif IR == MUL:
+                self.alu('MUL', operand_a, operand_b)
+                self.pc 
             
 
+            
